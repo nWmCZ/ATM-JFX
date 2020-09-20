41cbe273042f286a7747ed0a89ccec13f85c3d75
Stanislav Novotný <stanislav.novotny@unicorn.com>
2020-09-20 11:58:04 +0200

    GUI and Controller skeleton

---
+++ src/eu/atm/Controller.java
@@ -0,0 +1,154 @@
+package eu.atm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label regularAccountBalanceLabel;
+
+    @FXML
+    private TextField idTextBox;
+
+    @FXML
+    private TextField vstupniRok1;
+
+    @FXML
+    private Button b1Button;
+
+    @FXML
+    private Button b2Button;
+
+    @FXML
+    private Button b3Button;
+
+    @FXML
+    private Button b4Button;
+
+    @FXML
+    private Button b5Button;
+
+    @FXML
+    private Button b6Button;
+
+    @FXML
+    private Button b7Button;
+
+    @FXML
+    private Button b8Button;
+
+    @FXML
+    private Button b9Button;
+
+    @FXML
+    private Button b0Button;
+
+    @FXML
+    private Button asteriskButton;
+
+    @FXML
+    private Button hashtagButton;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Label savingsAccountBalanceLabel;
+
+    @FXML
+    private Label vysledek1;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    void button0_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button1_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button2_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button3_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button4_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button5_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button6_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button7_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button8_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button9_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonAsterisk_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonHastag_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ok_onAction(ActionEvent event) {
+
+    }
+
+}
